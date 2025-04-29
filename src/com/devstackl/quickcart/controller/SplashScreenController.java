8d02d75ce041ea91fd95cf94ee3b8afb22bcc0b8
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2025-04-29 15:27:18 +0530

    login form

--- src/com/devstackl/quickcart/controller/SplashScreenController.java
+++ src/com/devstackl/quickcart/controller/SplashScreenController.java
@@ -1,4 +1,52 @@
 package com.devstackl.quickcart.controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class SplashScreenController {
+
+    public ProgressBar progressbar;
+    public AnchorPane container;
+
+    public void initialize() {
+        load();
+    }
+
+    private void load() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO,new KeyValue(progressbar.progressProperty(),0)),
+                new KeyFrame(Duration.seconds(2),new KeyValue(progressbar.progressProperty(),1))
+        );
+        timeline.play();
+        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(2));
+        pauseTransition.setOnFinished(event -> {openLoginForm();});
+        pauseTransition.play();
+    }
+
+    private void openLoginForm() {
+        URL resource = getClass().getResource("../view/LoginForm.fxml");
+        Parent parent= null;
+        try {
+            parent = FXMLLoader.load(resource);
+            Scene scene=new Scene(parent);
+            Stage stage=(Stage) container.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Login Form");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
