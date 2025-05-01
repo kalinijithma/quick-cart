b2d90de558aa11a2dd2846e54e3ae58c24a6a445
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2025-05-01 14:53:01 +0530

    Dashboard

--- src/com/devstackl/quickcart/controller/ResetPassword.java
+++ src/com/devstackl/quickcart/controller/ResetPassword.java
@@ -1,8 +1,32 @@
 package com.devstackl.quickcart.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ResetPassword {
+    public AnchorPane container;
+    private void setUi(String location){
+        URL resource =
+                getClass().getResource("../view/"+location+".fxml");
+        Parent parent = null;
+        try {
+            parent = FXMLLoader.load(resource);
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) container.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Dashboard");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public void changePasswordController(ActionEvent actionEvent) {
+        setUi("Dashboard");
     }
 }
